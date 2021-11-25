5be9125d7d82bcfe29510471e85886b4053759db
ADAM <oumarov.adam@gmail.com>
2021-11-25 11:05:37 +0100

    Update Member.java

--- src/reseauSocial/core/Member.java
+++ src/reseauSocial/core/Member.java
@@ -10,4 +10,20 @@ public class Member {
 		this.localisation = localisation;
 	}
 
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getLocalisation() {
+		return localisation;
+	}
+
+	public void setLocalisation(String localisation) {
+		this.localisation = localisation;
+	}
+
 }
