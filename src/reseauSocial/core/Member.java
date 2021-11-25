b432290e82bed8644eab36080a85b9dcb3573811
ADAM <oumarov.adam@gmail.com>
2021-11-25 11:04:20 +0100

    update

---
+++ src/reseauSocial/core/Member.java
@@ -0,0 +1,13 @@
+package reseauSocial.core;
+
+public class Member {
+	
+	private String nom;
+	private String localisation;
+	
+	public void Mumber(String nom, String localisation) {
+		this.nom = nom;
+		this.localisation = localisation;
+	}
+
+}
---
+++ src/reseauSocial/core/SocialNetWorkImpl.java
@@ -0,0 +1,11 @@
+package reseauSocial.core;
+
+public class SocialNetWorkImpl extends Member{
+
+	private String nom;
+	
+	public void reseauSocial(String nom) {
+		this.nom = nom;
+	}
+	
+}
