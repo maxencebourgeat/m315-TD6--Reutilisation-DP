784b9175040e1d3ebc1812c736f86800655dc2c3
maxencebourgeat <90375017+maxencebourgeat@users.noreply.github.com>
2021-11-25 11:08:04 +0100

    Revert "Update SocialNetWorkImpl.java"
    This reverts commit a52d0e733a02e69598e04a91b09204897e98f550.

--- src/reseauSocial/core/SocialNetWorkImpl.java
+++ src/reseauSocial/core/SocialNetWorkImpl.java
@@ -7,13 +7,5 @@ public class SocialNetWorkImpl extends Member{
 	public void reseauSocial(String nom) {
 		this.nom = nom;
 	}
-
-	public String getNom() {
-		return nom;
-	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
 	
 }
