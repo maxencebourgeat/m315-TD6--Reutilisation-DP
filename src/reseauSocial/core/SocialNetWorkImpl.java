a52d0e733a02e69598e04a91b09204897e98f550
maxencebourgeat <90375017+maxencebourgeat@users.noreply.github.com>
2021-11-25 11:05:52 +0100

    Update SocialNetWorkImpl.java

--- src/reseauSocial/core/SocialNetWorkImpl.java
+++ src/reseauSocial/core/SocialNetWorkImpl.java
@@ -7,5 +7,13 @@ public class SocialNetWorkImpl extends Member{
 	public void reseauSocial(String nom) {
 		this.nom = nom;
 	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
 	
 }
